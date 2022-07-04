f99a20f95150865bb0bd7ff77bad54659636d997
Amit <d00417346@dmail.dixie.edu>
2022-07-04 19:14:54 -0400

    fixed wierd indenation for seasonal_info dict and changed name of mentor_info_just to mentor_info

--- scheduling/bad_db.py
+++ scheduling/bad_db.py
@@ -1,96 +1,95 @@
 import datetime as dt
 
 seasonal_shift_info = {
-					'summer': {
-							'dates': {
-									'start': dt.datetime(dt.date.today().year, 5, 1), 
-									'end':  dt.datetime(dt.date.today().year, 7, 31)
-									},
-							'shift_info': {
-								'Sunday': {
-									'a_shift': 9,
-									'b_shift': 9
-								}, 
-								'Monday': {
-									'a_shift': 6,
-									'b_shift': 7,
-									'c_shift': 3
-								},
-								'Tuesday': {
-									'a_shift': 7,
-									'b_shift': 7,
-									'c_shift': 3
-								},
+	'summer': {
+		'dates': {
+				'start': dt.datetime(dt.date.today().year, 5, 1), 
+				'end':  dt.datetime(dt.date.today().year, 7, 31)
+				},
+		'shift_info': {
+				'Sunday': {
+					'a_shift': 9,
+					'b_shift': 9
+					}, 
+				'Monday': {
+					'a_shift': 6,
+					'b_shift': 7,
+					'c_shift': 3
+				},
+				'Tuesday': {
+					'a_shift': 7,
+					'b_shift': 7,
+					'c_shift': 3
+				},
 								
-								'Wednesday': {
-									'a_shift': 7,
-									'b_shift': 7,
-									'c_shift': 3
-								},
-								'Thursday': {
-									'a_shift': 7,
-									'b_shift': 7,
-									'c_shift': 3
-								},
-								'Friday': {
-									'a_shift': 7,
-									'b_shift': 7,
-									'c_shift': 3
-								},
-								'Saturday': {
-									'a_shift': 7,
-									'b_shift': 7,
-									'c_shift': 3
-								},
-						},
-					},
+				'Wednesday': {
+					'a_shift': 7,
+					'b_shift': 7,
+					'c_shift': 3
+				},
+				'Thursday': {
+					'a_shift': 7,
+					'b_shift': 7,
+					'c_shift': 3
+				},
+				'Friday': {
+					'a_shift': 7,
+					'b_shift': 7,
+					'c_shift': 3
+				},
+				'Saturday': {
+					'a_shift': 7,
+					'b_shift': 7,
+					'c_shift': 3
+				},
+			},
+		},
 
-					'winter': {
-							'dates': {
-								'start': dt.datetime(dt.date.today().year, 8, 1), 
-								'end':  dt.datetime(dt.date.today().year + 1, 4, 30)
-							},
-							'shift_info': {
-								'Sunday': {
-									'a_shift': 9,
-									'b_shift': 9
-								}, 
-								'Monday': {
-									'a_shift': 6,
-									'b_shift': 6,
-									'c_shift': 3
-								},
-								'Tuesday': {
-									'a_shift': 6,
-									'b_shift': 6,
-									'c_shift': 3
-								},
-								
-								'Wednesday': {
-									'a_shift': 6,
-									'b_shift': 6,
-								},
-								'Thursday': {
-									'a_shift': 6,
-									'b_shift': 6,
-									'c_shift': 3
-								},
-								'Friday': {
-									'a_shift': 8,
-									'b_shift': 8,
-									'c_shift': 3
-								},
-								'Saturday': {
-									'a_shift': 11,
-									'b_shift': 11,
-									'c_shift': 4
-								},
-							},
-						},
-					}
+	'winter': {
+		'dates': {
+			'start': dt.datetime(dt.date.today().year, 8, 1), 
+			'end':  dt.datetime(dt.date.today().year + 1, 4, 30)
+			},
+		'shift_info': {
+				'Sunday': {
+					'a_shift': 9,
+					'b_shift': 9
+					}, 
+				'Monday': {
+					'a_shift': 6,
+					'b_shift': 6,
+					'c_shift': 3
+					},
+				'Tuesday': {
+					'a_shift': 6,
+					'b_shift': 6,
+					'c_shift': 3
+					},				
+				'Wednesday': {
+					'a_shift': 6,
+					'b_shift': 6,
+					},
+				'Thursday': {
+					'a_shift': 6,
+					'b_shift': 6,
+					'c_shift': 3
+				},
+				'Friday': {
+					'a_shift': 8,
+					'b_shift': 8,
+					'c_shift': 3
+				},
+				'Saturday': {
+					'a_shift': 11,
+					'b_shift': 11,
+					'c_shift': 4
+				},
+			},
+		},
+	}
 
 #June info
-mentor_info_june = {
+mentor_info = {
 	'Devon': {
 		'hard_dates': [i for i in range (2,6)] + [7] + [i for i in range (9,13)] + [14] + [i for i in range (16,20)] + [21] + [i for i in range (23,27)]+ [28] + [30],
 		'hours_wanted': 14,
