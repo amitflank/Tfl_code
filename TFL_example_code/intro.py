32516b4abaf10ef1c860c44b981da984c3b96a68
Amit <d00417346@dmail.dixie.edu>
2022-09-09 16:18:53 -0400

    finished last assignments

--- TFL_example_code/intro.py
+++ TFL_example_code/intro.py
@@ -2,7 +2,7 @@ from random import randint
 
 # topics loops, types, lists, conditionals, operators
 
-
+#This file includes the following program's
 # print even if number is even, otherwise print odd, repeat with list of numbers
 # print fizz if divisible by 3, buzz if divisible by 5 and fizzbuzz if divisible by both
 # check if a string is a palindrome
@@ -10,7 +10,6 @@ from random import randint
 # check if all values in a list are greater then 10
 # loop until we sum of all numbers greater than 100 with random numbers b/w 1 and 10
 # get the absolute value of a number
-# add next number to cumulative value if would not result in the new number being greater than 50
 # swap the values of 2 variables
 # raise one number to the power of another number
 
@@ -40,7 +39,7 @@ if num % 5 == 0:# loop until we randomly generate a number greater than 100
 print(word)
 
 
-# check if a string is a palindrome
+# check if a string is a palindrome, works for list as well, shows string is just a fancy list
 word = "101"
 start = 0 #get first letter location
 end = len(word) - 1 #get last letter location
@@ -57,13 +56,7 @@ for letter in word:
         break
 
 
-
-if is_pal:
-    print(word + "is a palindrome")
-else:
-    print(word + "is not a palindrome")
-
-
+ras
 # check if all values in a list are greater then 10
 my_list = [1,30, 288, 20, 11]
 
@@ -97,3 +90,23 @@ else:
     abs_num = num
 
 print("the absolute value of {0} is {1}".format(num, abs_num))
+
+#swap 2 numbers
+a = 20
+b= 10
+c = b
+b = a
+a = c
+print(a,b)
+
+
+
+# raise one number to the power of another number
+val = 2
+power = 4
+raised_val = 1
+
+for i in range(power):
+    raised_val *= val
+
+print(raised_val)
