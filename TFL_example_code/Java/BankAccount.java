b22493d006adcfd362d0d40c0373a5bc2b86ecdf
Amit <d00417346@dmail.dixie.edu>
2022-11-08 14:55:13 -0500

    stuff

--- TFL_example_code/Java/BankAccount.java
+++ TFL_example_code/Java/BankAccount.java
@@ -34,4 +34,37 @@ class BankAccount {
         this.balance += account.withdraw(value);
     }
 
+    /**transfer money from second account to first account, this is an example of method overloading.
+     * @param acc1 BankAccount we are transferring money to
+     * @param acc2 BankAccount we are withdrawing money from
+     * @param value amount of money being transferred
+     */
+    public static void transfer(BankAccount acc1, BankAccount acc2, int value){
+        acc1.deposit(acc2.withdraw(value));
+    }
+
+}
+
+class BankAccountExercise {
+    public static void main(String[] args) {
+        BankAccount jeff = new BankAccount("Jeff", 200);
+        BankAccount sue = new BankAccount("Sue", 100);
+
+        System.out.println(jeff.getBalance());
+        System.out.println(sue.getBalance());
+
+
+        jeff.transfer(sue, 150);
+        System.out.println(jeff.getBalance());
+        System.out.println(sue.getBalance());
+
+        sue.deposit(100);
+        jeff.transfer(sue, 150);
+
+        System.out.println(jeff.getBalance());
+        System.out.println(sue.getBalance());
+
+        sue.withdraw(-50);
+
+    }
 }
