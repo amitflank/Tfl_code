7e0050de34d1c84685225809d8214446e10f27bc
Amit <amitf82@gmail.com>
2024-10-17 12:41:00 -0600

    added user input for sentences

--- TFL_example_code/Java/AdlibGame.java
+++ TFL_example_code/Java/AdlibGame.java
@@ -1,4 +1,7 @@
 import java.util.Random;
+import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Set;
 
 public class AdlibGame {
 
@@ -8,7 +11,7 @@ public class AdlibGame {
         return strArr[index];
     }
 
-    public static void createSentence(String[] nouns, String[] verbs, String[] adjectives, String[] places, String[] objects) {
+    public static String createRandomSentence(String[] nouns, String[] verbs, String[] adjectives, String[] places, String[] objects) {
         String[] sentenceTemplates = {
             "The " + selectString(adjectives) + " " + selectString(nouns) + " " + selectString(verbs) + " " + selectString(places) + ".",
             "In the " + selectString(places) + ", a " + selectString(adjectives) + " " + selectString(nouns) + " was holding a " + selectString(objects) + ".",
@@ -16,22 +19,31 @@ public class AdlibGame {
             "The " + selectString(nouns) + " quickly " + selectString(verbs) + " with the " + selectString(objects) + " while everyone in the " + selectString(places) + " was watching."
         };
         
-        System.out.println(selectString(sentenceTemplates));
+        return selectString(sentenceTemplates);
     }
-    
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         // Word lists
         String[] nouns = {"Dragon", "Wizard", "Spaceship", "Robot", "Unicorn", "Ninja", "Pirate", "Alien", "Zombie", "Monster"};
         String[] verbs = {"Dances", "Flies", "Eats", "Jumps", "Builds", "Fights", "Explores", "Discovers", "Chases", "Escapes"};
         String[] adjectives = {"Silly", "Enormous", "Mysterious", "Brave", "Hungry", "Invisible", "Cuddly", "Weird", "Smelly", "Sparkly"};
         String[] places = {"In a haunted castle", "On a distant planet", "Inside a volcano", "At the bottom of the ocean", "In a magical forest",
                            "On a pirate ship", "In a candy factory", "Inside a video game", "At the zoo", "On top of a mountain"};
-        String[] objects = {"Magic wand", "Treasure chest", "Crystal ball", "Potion", "Map", "Sword", "Key", "Crown", "Amulet", "Book"};
+        String[] objects = {"Laser sword", "Magic wand", "Treasure chest", "Spacesuit", "Golden crown"};
 
+        System.out.println("How many sentences would you like to generate?");
+        int sentenceCount = scanner.nextInt();
+        Set<String> uniqueSentences = new HashSet<>();
 
-        for(int i = 0; i < 10; i++){
-            createSentence(nouns, verbs, adjectives, places, objects);
+        for (int i = 0; i < sentenceCount; i++) {
+            String newSentence = createRandomSentence(nouns, verbs, adjectives, places, objects);
+            if (uniqueSentences.add(newSentence)) {
+                System.out.println(newSentence);
+            }
         }
 
+        scanner.close();
     }
 }
